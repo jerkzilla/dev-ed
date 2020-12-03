40172fb542c699029ddeddae2715d2bddead7d87
jerkzilla <johnjamesmarshalljr@gmail.com>
2020-12-03 07:15:52 -0600

    using state and hooks

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import TweetList from "./components/TweetList";
 import CreateTweet from "./components/CreateTweet";
 
 function App() {
-  const name = "jj";
-  const message = "this tweet";
-
+  const [name, setName] = useState("jj");
+  const message = "hey girl";
   return (
     <div>
       <CreateTweet />
--- src/components/CreateTweet.js
+++ src/components/CreateTweet.js
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function CreateTweet() {
+  const [textInput, setTextInput] = useState("");
+
+  const userInputHandler = (e) => {
+    setTextInput(e.target.value);
+  };
+
   return (
     <div>
       <form>
-        <textarea cols="50" rows="5"></textarea>
+        <textarea
+          value={textInput}
+          onChange={userInputHandler}
+          cols="50"
+          rows="5"
+        ></textarea>
         <button>submit</button>
       </form>
     </div>
